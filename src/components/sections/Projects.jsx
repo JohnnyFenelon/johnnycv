641eba624c5032856cda5f7141f9abb0265f8848
Johnny F. <90154846+JohnnyFenelon@users.noreply.github.com>
2025-05-11 16:49:19 -0400

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -34,7 +34,7 @@ export const Projects = () => {
 
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://programacion.lat/"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
